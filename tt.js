daf51c58cfa813d08ed5ac75f964bf66be30ba36
Fang <942585437@qq.com>
2016-04-18 11:48:08 +0800

    添加markdown

---
+++ tt.js
@@ -0,0 +1,12 @@
+/*var arryAll = [];
+arryAll.push({age:1});
+arryAll.push({age:2});
+arryAll.push({age:3});
+arryAll.push({age:4});
+
+arryAll.forEach(function(e){
+    e.age=4;
+})
+console.log(arryAll)//[ { age: 4 }, { age: 4 }, { age: 4 }, { age: 4 } ]*/
+
+
